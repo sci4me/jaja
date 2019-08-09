3cc73b5b4c876f3826132bd49f50131c4374c221
Scitoshi Nakayobro <matthewtpeterson1@gmail.com>
2019-08-08 22:33:10 -0400

    Work on testing

--- src/array.cpp
+++ src/array.cpp
@@ -13,7 +13,96 @@ DEFINE_TEST(push_works) {
 }
 
 DEFINE_TEST(pop_works) {
+	auto a = Array<u32>();
 
+	for(u32 i = 0; i < 10; i++) a.push(i);
+
+	for(u32 i = 0; i < 10; i++) assert(a.pop() == 9 - i);
+}
+
+DEFINE_TEST(clear_works) {
+	auto a = Array<u32>();
+
+	a.push(42);
+	a.push(69);
+
+	assert(a.count == 2);
+
+	a.clear();
+
+	assert(a.count == 0);
+}
+
+DEFINE_TEST(unordered_remove_works) {
+	auto a = Array<u32>();
+
+	a.push(1);
+	a.push(2);
+	a.push(3);
+	a.push(4);
+	a.push(5);
+
+	assert(!a.unordered_remove(5));
+
+	assert(a.unordered_remove(1));
+	assert(a.unordered_remove(3));
+
+	assert(a.count == 3);
+
+	assert(a.data[0] == 1);
+	assert(a.data[1] == 5);
+	assert(a.data[2] == 3);
+}
+
+DEFINE_TEST(ordered_remove_works) {
+	auto a = Array<u32>();
+
+	a.push(1);
+	a.push(2);
+	a.push(3);
+	a.push(4);
+	a.push(5);
+
+	assert(!a.ordered_remove(5));
+
+	assert(a.ordered_remove(1));
+	assert(a.ordered_remove(3));
+
+	assert(a.count == 3);
+
+	assert(a.data[0] == 1);
+	assert(a.data[1] == 3);
+	assert(a.data[2] == 4);
+}
+
+DEFINE_TEST(index_of_works) {
+	auto a = Array<u32>();
+
+	for(u32 i = 0; i < 5; i++) a.push(4 - i);
+
+	for(u32 i = 0; i < 5; i++) assert(a.index_of(i) == 4 - i);
+
+	assert(a.index_of(42) == -1);
+}
+
+DEFINE_TEST(expands_as_needed) {
+	auto a = Array<u32>();
+
+	auto n = a.size;
+
+	for(u32 i = 0; i < n; i++) a.push(i);
+
+	assert(a.size == n * 2);
+}
+
+DEFINE_TEST(for_macro_works) {
+	auto a = Array<u32>();
+
+	for(u32 i = 0; i < 5; i++) a.push(i);
+
+	FOR((&a), i) {
+		assert(a.data[i] == i);
+	}
 }
 
 #endif
--- src/test_setup.cpp
+++ src/test_setup.cpp
@@ -5,4 +5,10 @@ extern void __setup_test(const char *name, void (*fn)());
 void test_setup() {
 extern void _push_works(); SETUP_TEST(push_works);
 extern void _pop_works(); SETUP_TEST(pop_works);
+extern void _clear_works(); SETUP_TEST(clear_works);
+extern void _unordered_remove_works(); SETUP_TEST(unordered_remove_works);
+extern void _ordered_remove_works(); SETUP_TEST(ordered_remove_works);
+extern void _index_of_works(); SETUP_TEST(index_of_works);
+extern void _expands_as_needed(); SETUP_TEST(expands_as_needed);
+extern void _for_macro_works(); SETUP_TEST(for_macro_works);
 }
