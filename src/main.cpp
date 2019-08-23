ecad61acfadf5550cac7bfb1a401ce98d71248c4
Scitoshi Nakayobro <matthewtpeterson1@gmail.com>
2019-08-22 22:59:45 -0400

    Brainf*ck works, quite well. Learned some. Library is almost as easy to use as myjit/lightning but MUCH better in all ways.

--- src/main.cpp
+++ src/main.cpp
@@ -13,19 +13,144 @@
 #include <stdlib.h>
 #include <jit/jit.h>
 #include <jit/jit-type.h>
+#include <jit/jit-dump.h>
 
 static void compile_and_run(char *source) {
 	auto ctx = jit_context_create();
 	
 	jit_context_build_start(ctx);
 
-	auto signature = jit_type_create_signature(jit_abi_cdecl, jit_type_void, 0, 0, 0);
-	auto func = jit_function_create(ctx, signature);
+	jit_type_t putchar_params[] = { jit_type_int };
+	auto putchar_signature = jit_type_create_signature(jit_abi_cdecl, jit_type_int, putchar_params, 1, 0);
+	auto getchar_signature = jit_type_create_signature(jit_abi_cdecl, jit_type_int, 0, 0, 0);
 
-	jit_function_compile(func);
+	auto signature = jit_type_create_signature(jit_abi_cdecl, jit_type_void, 0, 0, 0);
+	auto j = jit_function_create(ctx, signature);
+
+	auto tapeSize = jit_value_create_nint_constant(j, jit_type_int, 30000);
+	auto tape = jit_insn_alloca(j, tapeSize);
+	auto zero = jit_value_create_nint_constant(j, jit_type_int, 0);
+	jit_insn_memset(j, tape, zero, tapeSize);
+
+	auto jit_type_ubyte_ptr = jit_type_create_pointer(jit_type_ubyte, 0);
+	
+	auto dp = jit_value_create(j, jit_type_ubyte_ptr);
+	jit_insn_store(j, dp, tape);
+
+	Array<jit_label_t> loop_starts;
+	Array<jit_label_t> loop_ends;
+
+	auto len = strlen(source);
+	auto i = 0;
+	while(i < len) {
+		auto c = source[i];
+		switch(c) {
+			case '+':
+			case '-':
+			case '>':
+			case '<': {
+				auto n = 1;
+
+				{
+					auto j = i + 1;
+					while(j < len && source[j] == c) {
+						j++;
+						n++;
+					}
+					i = j;
+				}
+
+				auto count = jit_value_create_nint_constant(j, jit_type_int, n);
+
+				if(c == '+' || c == '-') {
+					auto old = jit_insn_load_relative(j, dp, 0, jit_type_ubyte);
+					jit_value_t x;
+					if(c == '+') {
+						x = jit_insn_add(j, old, count);
+					} else {
+						x = jit_insn_sub(j, old, count);
+					}
+					auto y = jit_insn_convert(j, x, jit_type_ubyte, 0);
+					jit_insn_store_relative(j, dp, 0, y);
+				} else {
+					jit_value_t x;
+					if(c == '>') {
+						x = jit_insn_add(j, dp, count);
+					} else {
+						x = jit_insn_sub(j, dp, count);
+					}
+					jit_insn_store(j, dp, x);
+				}
+				break;
+			}
+			case '.': {
+				auto x = jit_insn_load_relative(j, dp, 0, jit_type_ubyte);
+				jit_insn_call_native(j, "putchar", (void*)putchar, putchar_signature, &x, 1, JIT_CALL_NOTHROW);				
+
+				i++;
+				break;
+			}
+			case ',': {
+				// TODO
+				i++;
+				break;
+			}
+			case '[': {
+				if(i + 2 < len && source[i + 1] == '-' && source[i + 2] == ']') {
+					auto x = jit_insn_convert(j, zero, jit_type_ubyte, 0);
+					jit_insn_store_relative(j, dp, 0, x);
+
+					i += 3;
+				} else {
+					auto x = jit_insn_load_relative(j, dp, 0, jit_type_ubyte);
+					auto c = jit_insn_eq(j, x, zero);
+
+					auto end = jit_label_undefined;
+					jit_insn_branch_if(j, c, &end);
+					loop_ends.push(end);
+
+					jit_label_t start = jit_label_undefined;
+					jit_insn_label(j, &start);
+					loop_starts.push(start);
+
+					i++;		
+				}		
+				break;
+			}
+			case ']': {
+				auto start = loop_starts.pop();
+				auto end = loop_ends.pop();
+
+				auto x = jit_insn_load_relative(j, dp, 0, jit_type_ubyte);
+				auto c = jit_insn_ne(j, x, zero);
+				jit_insn_branch_if(j, c, &start);
+
+				jit_insn_label(j, &end);
+
+				i++;
+				break;
+			}
+			default: {
+				i++;
+				break;
+			}
+		}
+	}
+
+	assert(!loop_starts.count);
+
+	jit_insn_return(j, 0);
+
+	// printf("\n------------------------------------------------------------\n\n");
+	// jit_dump_function(stdout, j, "main");
+	jit_function_compile(j);
+	// printf("------------------------------------------------------------\n\n");
+	// jit_dump_function(stdout, j, "main");
 	jit_context_build_end(ctx);
 
-	jit_function_apply(func, 0, 0);
+	// printf("------------------------------------------------------------\n\n");
+
+	jit_function_apply(j, 0, 0);
 
 	jit_context_destroy(ctx);	
 }
